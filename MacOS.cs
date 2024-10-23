182ebca9bdea07dbc21cda45e609f292fbf79a79
wirisoft <wirideveloper@gmail.com>
2024-10-23 15:36:11 -0600

    abstract factory

---
+++ MacOS.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _03AbstractFactory
+{
+    public class MacOS:IOperatingSystem
+    {
+        public string Name => "MacOS";
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using _03AbstractFactory;
+
+Console.WriteLine("Abstract Factory");
+
+OperatingSystemFactory factory = new WindowsFactory();
+IOperatingSystem operatingSystem= factory.CreateOperatingSystem();
+
+Console.WriteLine($"Operating System: {operatingSystem.Name}");
+
+factory = new MacOSFactory();
+operatingSystem = factory.CreateOperatingSystem();
+Console.WriteLine($"operatingSystem: {operatingSystem.Name}");
